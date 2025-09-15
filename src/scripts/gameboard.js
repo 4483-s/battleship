6a915ff6ee76d80398580f33406cb0508e80608e
Hermann <188955756+4483-s@users.noreply.github.com>
2025-09-15 15:14:10 +0800

    refactor placeship

--- src/scripts/gameboard.js
+++ src/scripts/gameboard.js
@@ -4,38 +4,11 @@ export default class Gameboard {
     this.board = {};
     this.ships = {};
   }
-  #ckeckIsVertical(start, end) {
-    return start[0] === end[0];
-  }
-  placeShip(start, end) {
-    const isVertical = this.#ckeckIsVertical(start, end);
-    // const shipLength = Math.abs(
-    //   isVertical ? start[1] - end[1] : start[0] - end[0],
-    // );
-    const ship = new Ship();
-    this.ships[ship.id] = ship;
-    let shipLength = 0;
-    if (isVertical) {
-      const x = start[0];
-      const sorted = [start, end].sort((a, b) => a[1] - b[1]);
-      for (let i = sorted[0][1]; i <= sorted[1][1]; i++) {
-        this.board[`${x}${i}`] = ship.id;
-        shipLength++;
-      }
-    } else {
-      const y = start[1];
-      const sorted = [start, end].sort((a, b) => a[0] - b[0]);
-      for (let i = sorted[0][0]; i <= sorted[1][0]; i++) {
-        this.board[`${y}${i}`] = ship.id;
-        shipLength++;
-      }
-    }
-    ship.length = shipLength;
-  }
   receiveAttack(x, y) {
     if (board[`${x}${y}`]) return;
   }
 }
+class Coordinate {}
 const isValidCoordinate = (x, y, size) =>
   x >= 0 && x < size && y >= 0 && y < size;
 
